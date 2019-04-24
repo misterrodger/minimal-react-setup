504909971b929940f706bccb90b1125bd252d9bd
misterrodger <johndavidrodger@gmail.com>
2019-04-24 16:53:14 +0300

    Add files from other repo
    -Migrated files from other repo that wasn't initialized properly
    -Hope to fix issues with dependencies, etc
    Relates #1

---
+++ src/components/input/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class Form extends React.Component {
+  state = { birthday: ""}
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const data = JSON.stringify(this.state);
+    console.log("Submitting test");
+    this.setState({birthday: ""});
+  };
+
+  render() {
+    return (
+      <form onSubmit = {this.handleSubmit}>
+      <fieldset>
+      <legend>Find your Birthday Twins</legend>Birthday:
+      <input type="date" name="bday" value={this.state.birthday} />
+      <input type="submit" name="submit" />
+      </fieldset>
+      </form>
+    )
+  }
+}
+
+ReactDOM.render(<Form />, root);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// ReactDOM.render(<div>hello world</div>, document.getElementById('🤡'));
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { getData } from './utils/data_helpers';
+import Form from './components/input/index.js';
 
-ReactDOM.render(<div>hello world</div>, document.getElementById('🤡'));
+class Card extends React.Component {
+    state = {
+        data: {}
+    };
+
+    componentDidMount() {
+        // getData('http://history.muffinlabs.com/date/01/01').then(data => console.log(data));
+        getData('http://history.muffinlabs.com/date/01/01').then(data => this.setState({ data }));
+    }
+
+    render() {
+        const {
+            data,
+            url,
+        } = this.state.data;
+
+        if( data ){
+            console.log("satra",data['Births']);
+            var y = data['Births'].length;
+            // var x = this.state.data.data['Briths'].text;
+            console.log(data['Births'][y-1].text);
+        }
+
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Card />, document.getElementById('root'));
