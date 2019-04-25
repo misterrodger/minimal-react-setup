4be7ec7dfa497c4b7a6113795c6c9b425c6501b2
misterrodger <johndavidrodger@gmail.com>
2019-04-25 12:30:37 +0300

    Fix bugs with api fetch
    -Fetch now working with Github api, need to get to work with history api
    Relates #3

--- src/utils/data_helpers.js
+++ src/utils/data_helpers.js
@@ -1,15 +1,18 @@
 export const API_BASE = "http://history.muffinlabs.com/date/01/01"
+// export const API_BASE = "https://api.github.com/users/misterrodger?access_token=72f21f76c68988867e1df798d69bc1ff2cf9dee5"
 
 const checkResponse = response => {
-  if (response.status != 200) {
+  if (response.status !== 200) {
     console.log((`Error with the request ${response.status}`));
     return;
   }
   return response.json();
+  // return response;
 }
 
 export const getData = url => {
-  return fetch(`${url}`)
+  return fetch(`http://history.muffinlabs.com/date/01/01`)
+  // return fetch(`https://api.github.com/users/misterrodger?access_token=72f21f76c68988867e1df798d69bc1ff2cf9dee5`)
   .then(checkResponse)
   .catch(err => {
     throw new Error (`fetch getUserData failed ${err}`);
